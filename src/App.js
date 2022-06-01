6617ca1e530a559a4e5bc70ea3153686c0629a91
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-01 18:17:28 +0700

    tugas sesi 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, { Component } from "react";
-import Footer from "./Page/Footer";
-import Header from "./Page/Header";
-import MenuKontak from "./Page/MenuKontak";
 import MenuMakanan from "./Page/MenuMakanan";
-import MenuTentangKami from "./Page/MenuTentangKami";
-
-
 class App extends Component {
 	render() {
 		return(
-			<div>
-				<Header/>
-				<MenuMakanan/>
-
-				<MenuTentangKami/>
-				<MenuKontak/>
-				<Footer/>
-			</div>
+			<MenuMakanan />
 		)
 	}
 }
